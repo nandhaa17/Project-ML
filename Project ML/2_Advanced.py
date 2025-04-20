7e31c386f15f50433b0e16b864169fd3795ea06c
Nandha <nandhadoc7@gmail.com>
2025-04-20 13:52:30 +0530

    Update 2_Advanced.py

--- Project ML/2_Advanced.py
+++ Project ML/2_Advanced.py
@@ -147,7 +147,7 @@ except:
     st.caption("**:red[_Please fill out the details_ !]**")
 
 import pandas as pd
-df=pd.read_csv('heart.csv')
+df=pd.read_csv('heart1.csv')
 from sklearn.model_selection import train_test_split
 x=df.iloc[:,0:13]
 
